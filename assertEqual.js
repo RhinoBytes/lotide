02e139f851f46bcb51096f1b4a07cd0e113ec82d
Doug Ross <dross.canada@gmail.com>
2023-03-23 21:31:09 -0400

    added assertEqual function

--- assertEqual.js
+++ assertEqual.js
@@ -1,1 +1,14 @@
-gergerg
+// Function Implementation
+
+const assertEqual = function(actual, expected) {
+  //if true print green check marks
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: [" + actual + "] === [" + expected + "]");
+  // if false print red stop symbols
+  } else {
+    console.log("🔴🔴🔴 Assertion Failed: [" + actual + "] !== [" + expected + "]");
+  }
+};
+// only used to test if its working
+assertEqual(7, 3);
+assertEqual("1", "1");
