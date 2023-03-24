0d5cd5c9a6bb46d5f4ed9c7724b42522b9f83452
Doug Ross <dross.canada@gmail.com>
2023-03-23 21:41:28 -0400

    updated with Template Literals

--- assertEqual.js
+++ assertEqual.js
@@ -3,10 +3,10 @@
 const assertEqual = function(actual, expected) {
   //if true print green check marks
   if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed: [" + actual + "] === [" + expected + "]");
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
   // if false print red stop symbols
   } else {
-    console.log("🔴🔴🔴 Assertion Failed: [" + actual + "] !== [" + expected + "]");
+    console.log(`🔴🔴🔴 Assertion Failed: [${actual}] !== [${expected}]`);
   }
 };
 // only used to test if its working
