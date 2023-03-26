8824706525e396044c48e725cdcdba0d3273c92f
Doug Ross <dross.canada@gmail.com>
2023-03-26 00:26:08 -0400

    added flatten.js file

---
+++ flatten.js
@@ -0,0 +1,27 @@
+// function that comparest two arrays by converting them to strings
+let eqArrays = function(first, second) {
+  return first.toString() === second.toString();
+};
+
+// function that logs to console if the two compared arrays are equal
+let assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+let flatten = function(arrays) {
+  let newArray = [];
+  for (let i = 0; i < arrays.length; i++) {
+    if (Array.isArray(arrays[i])) {
+      newArray = newArray.concat(flatten(arrays[i]));
+    } else {
+      newArray.push(arrays[i]);
+    }
+  }
+  return newArray;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
