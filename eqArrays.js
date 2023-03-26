4f71212799e059c51820d6c71c8f40a0c5dd0211
Doug Ross <dross.canada@gmail.com>
2023-03-25 22:42:21 -0400

    added eqArrays.js file, for comparing two arrays

---
+++ eqArrays.js
@@ -0,0 +1,19 @@
+// Function Implementation
+
+const assertEqual = function(actual, expected) {
+  //if true print green check marks
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  // if false print red stop symbols
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+
+let eqArrays = function(first, second) {
+  return first.toString() === second.toString();
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 5], [1, 2, 3]), true);
