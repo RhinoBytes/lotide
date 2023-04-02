509d93c9c1af3566b220259506e30572a6758394
Doug Ross <dross.canada@gmail.com>
2023-04-02 15:11:31 -0400

    added findKeysByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,35 @@
+const assertEqual = function(actual, expected) {
+  //if true print green check marks
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  // if false print red stop symbols
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+
+const findKeyByValue = function(object, value) {
+  let finalValue = undefined;
+  let objectKeys = Object.keys(object);
+  for (const key in object) {
+   if (object[key] === value) {
+      finalValue = key;
+      break;
+    } 
+    }
+    
+    return finalValue;
+};
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
