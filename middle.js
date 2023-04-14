d62922573446f238227221b35301cec4b0dbedac
Doug Ross <dross.canada@gmail.com>
2023-04-13 21:08:01 -0400

    added testing to middleTest.js to test middle.js

--- middle.js
+++ middle.js
@@ -1,18 +1,3 @@
-// function that comparest two arrays by converting them to strings
-let eqArrays = function(first, second) {
-  return first.toString() === second.toString();
-};
-
-// function that logs to console if the two compared arrays are equal
-let assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
-
 // function that returns middle of array
 const middle = function(array) {
   // set empty array to newArray
@@ -32,14 +17,4 @@ const middle = function(array) {
   }
   return newArray;
 };
-
-// test code
-
-const actual1 = middle([1, 2, 3, 4, 5]);
-const expected1 = [3];
-assertArraysEqual(actual1, expected1);
-
-
-const actual2 = middle([3, 7 ,8, 14, 11, 15])
-const expected2 = [8, 11];
-assertArraysEqual(actual1, expected1);
+module.exports = middle;
